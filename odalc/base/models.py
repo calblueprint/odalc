3781a2a1fb3bf8eade5426fe1bb8ac90c8921940
Byronium <byron.yi.zhang@gmail.com>
2014-02-27 13:29:04 -0800

    Update models.py

--- odalc/base/models.py
+++ odalc/base/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 # Create your models here.
+class Course(models.Model):
+  teacher = models.ForeignKey(TeacherUser)
+  students = models.ManyToMany(StudentUsers)
+  
+  title = models.CharField(max_length=50)
+  description = models.CharField(max_length=250)
+  size = models.IntegerField()
+  start_datetime = models.DateTimeField(blank=True,null=True)
+  end_datetime = models.DateTimeField(blank=True,null=True)
+  
+  prereqs = models.CharField(max_length=100)
+  skill_level = models.CharField(choices=(('Beginner','Beginner'),('Intermediate','Intermediate'),('Advanced','Advanced')))
+  cost = models.FloatField(validators=MinValueValidator(5.00))
+  odalc_cost_split = models.FloatField()
+
+  need_flyer = models.BooleanField()  
+  flyer = 
+  course_material = 
+  
+  additional_info = models.CharField()
+  
+
+  
