fbc481539da27b89c4183f2032617eed04369207
John Du <johnldu@gmail.com>
2014-02-27 20:17:03 -0800

    editing student, teacher

--- odalc/base/models.py
+++ odalc/base/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    email = models.EmailField("Email", max_length=255, unique=True)
+    password = None
+
+    class Meta:
+    	abstract = True
--- odalc/teachers/models.py
+++ odalc/teachers/models.py
@@ -1,10 +1,17 @@
 from django.db import models
-from django.contrib.auto import User
+from base import User
 
 # Create your models here.
-class Student(User):
+class Teacher(User):
     reviews = models.OneToManyField(Review, verbose_name="Reviews", null=True, blank=True)
     classes = models.OneToManyField(Class, verbose_name="Classes", null=True, blank=True)
 
     class Meta:
-        verbose_name = "Instructor"
+        verbose_name = "Teacher"
+
+# class Review(models.Model):
+#     instructor = models.ForeignKey("Teacher")
+#     student = models.ForeignKey("Student")
+#     course = models.ForeignKey("Course")
+#     rating = models.IntegerField("Rating")
+#     comments = models.TextField("Comments")
