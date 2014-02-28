501560f982a6c0fa957dd899608440fb415d7d1a
vivekraghuram <vv9295@yahoo.com>
2014-02-27 21:14:24 -0800

    Add extra multiple choice options to course feedback model and add titles to textfields in course feedback model

--- odalc/students/models.py
+++ odalc/students/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class CourseFeedback(models.Model):
-	first_name = models.ForeignKey('StudentUser')
+	student = models.ForeignKey('StudentUser')
 	course = models.ForeignKey('base.Course')
 
 	encourage_questions = models.BooleanField()
@@ -11,10 +11,15 @@ class CourseFeedback(models.Model):
 	time_length_choices = ('too_long', 'Too Long'),('too_short','Too Short'),('just_right','Just Right')
 	time_length = models.CharField(max_length=9, choices=time_length_choices)
 
-	satisfaction = models.TextField()
-	course_content = models.TextField()
-	course_organization = models.TextField()
-	teacher_delivery = models.TextField()
-	classroom_facilities = models.TextField()
-	useful_topics = models.TextField()
-	other_topics = models.TextField()
+#Change these later when adding more multiple choice fields to the model
+	multiple_choice_1 = models.CharField(max_length=255,)
+	multiple_choice_2 = models.CharField(max_length=255,)
+	multiple_choice_3 = models.CharField(max_length=255,)
+
+	satisfaction = models.TextField('Overall Satisfaction:')
+	course_content = models.TextField('Course Content:')
+	course_organization = models.TextField('Course Organization:')
+	teacher_delivery = models.TextField("Teacher's delivery")
+	classroom_facilities = models.TextField('Classroom Facilities')
+	useful_topics = models.TextField('Some of the most useful topics covered were:')
+	other_topics = models.TextField('What other topics would you like to see offered?')
