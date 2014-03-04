79348cc45c848d56280628c4b3177c8b044f93c9
John Du <johnldu@gmail.com>
2014-03-03 23:55:25 -0800

    update student/teacher models

--- odalc/base/models.py
+++ odalc/base/models.py
@@ -12,7 +12,7 @@ class User(AbstractBaseUser):
     objects = UserManager()
 
 class UserManager(BaseUserManager):
-	def create_user(self, email, first_name, last_name, password=None):
+    def create_user(self, email, first_name, last_name, password=None):
         """
         Creates and saves a User with the given email, date of
         birth and password.
@@ -27,5 +27,10 @@ class UserManager(BaseUserManager):
         )
 
         user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, first_name, last_name, password=None):
+        user = self.create_user(email, first_name, last_name, password=None)
         user.save(using=self._db)
         return user
