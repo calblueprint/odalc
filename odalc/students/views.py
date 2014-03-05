dce1bc2e696e241efd8676d23f0f29c20fa12d67
vivekraghuram <vv9295@yahoo.com>
2014-03-05 14:36:00 -0800

    Create basic feedback form using modelform

--- odalc/students/views.py
+++ odalc/students/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from odalc.students.models import CourseFeedback
+from django.views.generic import CreateView
 
 # Create your views here.
+
+class SubmitCourseFeedback(CreateView):
+	model = CourseFeedback
+	template_name = 'course_feedback_form.html'
+
+	def get_success_url(self):
+		return reverse('submit-success')
