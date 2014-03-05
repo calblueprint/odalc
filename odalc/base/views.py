784f76a7e27dc6bbfe1e0f717c07e893fe1bb0ad
vivekraghuram <vv9295@yahoo.com>
2014-03-05 14:56:56 -0800

    Make basic course creation view using modelform

--- odalc/base/views.py
+++ odalc/base/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from base.models import Course
 
 # Create your views here.
+
+class CreateCourse(CreateView):
+	model = Course
+	template_name = 'create_course_form.html'
+
+	def get_success_url(self):
+		return reverse('submit-success')
