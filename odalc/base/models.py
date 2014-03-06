f6f5db4dea74b64ea66ecc9e8d3152e20c52fb3d
vivekraghuram <vv9295@yahoo.com>
2014-03-05 22:51:53 -0800

    Move course creation to teacher app and crete course creation template

--- odalc/base/models.py
+++ odalc/base/models.py
@@ -40,20 +40,20 @@ class User(AbstractBaseUser):
 class Course(models.Model):
   teacher = models.ForeignKey('teachers.TeacherUser')
   students = models.ManyToManyField('students.StudentUser')
-  
+
   title = models.CharField(max_length=50)
   description = models.TextField()
   size = models.IntegerField()
   start_datetime = models.DateTimeField(blank=True,null=True)
   end_datetime = models.DateTimeField(blank=True,null=True)
-  
+
   prereqs = models.CharField(max_length=100)
   skill_level = models.CharField(max_length=25,choices=(('BEG','Beginner'),('INT','Intermediate'),('ADV','Advanced')))
   cost = models.DecimalField(max_digits=5,decimal_places=2,validators=[MinValueValidator(5.00)])
   odalc_cost_split = models.DecimalField(max_digits=5,decimal_places=2)
 
-  need_flyer = models.BooleanField()  
+  need_flyer = models.BooleanField()
   flyer = models.FileField(upload_to='Flyer Uploads')
   course_material = models.FileField(upload_to='Course Material')
-  
+
   additional_info = models.TextField(blank=True)
--- odalc/teachers/views.py
+++ odalc/teachers/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from odalc.base.models import Course
 
 # Create your views here.
+class CreateCourse(CreateView):
+	model = Course
+	template_name = 'teachers/create_course_form.html'
