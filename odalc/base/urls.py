1a4f2c00f825a3312b2445335aa155e18a36a9fe
vivekraghuram <vv9295@yahoo.com>
2014-03-12 23:34:39 -0700

    Fix primary key url for feedback form

--- odalc/base/urls.py
+++ odalc/base/urls.py
@@ -3,6 +3,6 @@ from odalc.base.views import CourseDetailView
 from odalc.students.views import SubmitCourseFeedbackView
 
 urlpatterns = patterns('',
-    url(r'^(?P<pk>\d+)/feedback/$', SubmitCourseFeedback.as_view(), name='feedback'),
+    url(r'^(?P<pk>\d+)/feedback/$', SubmitCourseFeedbackView.as_view(), name='feedback'),
     url(r'^(?P<pk>\d+)/$', CourseDetailView.as_view(), name='detail'),
 )
--- odalc/students/views.py
+++ odalc/students/views.py
@@ -13,11 +13,15 @@ class SubmitCourseFeedbackView(CreateView):
 	success_url = reverse_lazy('home')
 
 	def form_valid(self, form):
-
 		course_feedback = form.save(commit=False)
 		pk = self.kwargs.get('pk', None)
 		course_feedback.course = Course.objects.get(pk=pk)
 		course_feedback.student = Students.objects.order_by('?').first()
 		course_feedback.save()
+		return redirect(self.get_success_url())
 
-        return redirect(self.get_success_url())
+
+	def get_context_data(self, **kwargs):
+		context = super(SubmitCourseFeedbackView, self).get_context_data(**kwargs)
+		context['pk'] = self.kwargs.get('pk', None)
+		return context
