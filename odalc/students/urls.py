0ec389a4d35a55b9d762445d129072de1c10b36d
vivekraghuram <vv9295@yahoo.com>
2014-03-12 22:48:49 -0700

    Add course and random student to course feedback form

--- odalc/students/urls.py
+++ odalc/students/urls.py
@@ -2,5 +2,4 @@ from django.conf.urls import patterns, include, url
 from odalc.students.views import SubmitCourseFeedback
 
 urlpatterns = patterns('',
-	url(r'^feedback$', SubmitCourseFeedback.as_view(), name='feedback')
 )
--- odalc/students/views.py
+++ odalc/students/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import CreateView
 from odalc.students.forms import FeedbackForm
 from odalc.students.models import CourseFeedback
@@ -6,8 +6,18 @@ from django.core.urlresolvers import reverse_lazy
 
 # Create your views here.
 
-class SubmitCourseFeedback(CreateView):
+class SubmitCourseFeedbackView(CreateView):
 	model = CourseFeedback
 	template_name = 'students/course_feedback_form.html'
 	form_class = FeedbackForm
 	success_url = reverse_lazy('home')
+
+	def form_valid(self, form):
+
+		course_feedback = form.save(commit=False)
+		pk = self.kwargs.get('pk', None)
+		course_feedback.course = Course.objects.get(pk=pk)
+		course_feedback.student = Students.objects.order_by('?').first()
+		course_feedback.save()
+
+        return redirect(self.get_success_url())
