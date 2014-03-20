7211411067045b6d3d3fa7f4e92e17be4ea74a03
vivekraghuram <vv9295@yahoo.com>
2014-03-19 22:32:52 -0700

    Add file opening and json reading code

--- odalc/mailer.py
+++ odalc/mailer.py
@@ -1,4 +1,15 @@
-from django.core.mail import send_mail
+import os, json
+from django.core.mail import send_mail, send_mass_mail
+from django.conf.settings import SETTINGS_PATH
 
-send_mail('Subject here', 'Here is the message.', 'from@example.com',
-    ['to@example.com'], fail_silently=False)
+EMAIL_TEMPLATES_PATH = os.path.join(SETTINGS_PATH, 'templates', 'emails')
+DEFAULT_EMAIL = 'odalc@odalc.org'
+
+def send_odalc_emails(template_name, sender=DEFAULT_EMAIL, recipient_list):
+	template = open(os.path.join(EMAIL_TEMPLATES_PATH, template_name))
+	template_data = json.load(template)
+	template.close()
+
+
+
+	send_mail(subject, body, sender, recipient_list, fail_silently=False)
