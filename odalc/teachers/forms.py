bdf3991b6da6cd64d70a17c17af7277319d61dbe
John Du <johnldu@gmail.com>
2014-03-19 22:34:23 -0700

    commit_count++

--- odalc/teachers/forms.py
+++ odalc/teachers/forms.py
@@ -1,11 +1,34 @@
+from django.utils.translation import ugettext as _
 from django import forms
 from localflavor.us import forms as localflavor_forms
-
 from odalc.teachers.models import TeacherUser
 
 class TeacherCreateForm(forms.ModelForm):
-    phone = localflavor_forms.USPhoneNumberField()
+    """
+    A form that creates a user, with no privileges, from the given username and
+    password.
+    """
+    error_messages = {
+        'duplicate_username': _("A user with that username already exists."),
+        'password_mismatch': _("The two password fields didn't match."),
+    }
+    password1 = forms.CharField(label=_("Password"),
+        widget=forms.PasswordInput)
+    password2 = forms.CharField(label=_("Password confirmation"),
+        widget=forms.PasswordInput,
+        help_text=_("Enter the same password as above, for verification."))
+    phone = localflavor_forms.USPhoneNumberField(required=True, label='Phone')
+    zipcode = localflavor_forms.USZipCodeField(required=True, label='Zipcode')
 
     class Meta:
         model = TeacherUser
-        exclude = ['phone', 'last_login']
+        fields = ('email',
+                  'street_address',
+                  'city',
+                  'zipcode',
+                  'phone',
+                  'about',
+                  'picture',
+                  'resume',
+                  'experience',
+                  'info_source')
--- odalc/teachers/views.py
+++ odalc/teachers/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.views.generic import CreateView
 from odalc.teachers.forms import TeacherCreateForm
 from odalc.teachers.models import TeacherUser
+from django.core.urlresolvers import reverse_lazy
 
 # Create your views here.
 class TeacherRegisteration(CreateView):
-	model = TeacherUser
-	template_name = "teachers/teacher_register.html"
-	form_class = TeacherCreateForm
+    model = TeacherUser
+    template_name = "teachers/teacher_register.html"
+    form_class = TeacherCreateForm
+    success_url = reverse_lazy('home')
