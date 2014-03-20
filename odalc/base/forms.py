c6d8d79a1fecee9487df63bf3112f8f76144dcff
John Du <johnldu@gmail.com>
2014-03-19 23:29:36 -0700

    jdu++

---
+++ odalc/base/forms.py
@@ -0,0 +1,13 @@
+from django.utils.translation import ugettext as _
+from django import forms
+
+class UserRegisterForm(forms.ModelForm):
+    error_messages = {
+        'duplicate_username': _("A user with that username already exists."),
+        'password_mismatch': _("The two password fields didn't match."),
+    }
+    password1 = forms.CharField(label=_("Password"),
+        widget=forms.PasswordInput)
+    password2 = forms.CharField(label=_("Password confirmation"),
+        widget=forms.PasswordInput,
+        help_text=_("Enter the same password as above, for verification."))
