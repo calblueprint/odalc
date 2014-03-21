7565cd42ea746a3170c96b4a3d8562e5e1ea36be
vivekraghuram <vv9295@yahoo.com>
2014-03-20 20:17:05 -0700

    @vivek++ Fix some errors in mailer and successfully test that it works

--- odalc/mailer.py
+++ odalc/mailer.py
@@ -1,12 +1,12 @@
 import os, json
 from django.core.mail import send_mail, send_mass_mail
-from django.conf.settings import SETTINGS_PATH
+from django.conf import settings
 from django.template import Template, Context
 
-EMAIL_TEMPLATES_PATH = os.path.join(SETTINGS_PATH, 'templates', 'emails')
+EMAIL_TEMPLATES_PATH = os.path.join(settings.SETTINGS_PATH, 'templates', 'emails')
 DEFAULT_EMAIL = 'odalc@odalc.org'
 
-def send_odalc_emails(template_name, context_dict, sender=DEFAULT_EMAIL, recipient_list):
+def send_odalc_emails(template_name, context_dict, recipient_list, sender=DEFAULT_EMAIL):
 	with open(os.path.join(EMAIL_TEMPLATES_PATH, template_name), 'r') as template:
 		template_data = json.load(template)
 	context = Context(context_dict)
