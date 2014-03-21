a8592b9eb07a4ec523cd79c546a1461b855a4a91
vivekraghuram <vv9295@yahoo.com>
2014-03-20 20:30:32 -0700

    vivek++ Modify mailer to read all emails from same json

--- odalc/mailer.py
+++ odalc/mailer.py
@@ -3,15 +3,15 @@ from django.core.mail import send_mail
 from django.conf import settings
 from django.template import Template, Context
 
-EMAIL_TEMPLATES_PATH = os.path.join(settings.SETTINGS_PATH, 'templates', 'emails')
+EMAIL_TEMPLATES_PATH = os.path.join(settings.SETTINGS_PATH, 'templates', 'emails', 'basic_email.json')
 DEFAULT_EMAIL = 'odalc@odalc.org'
 
 def send_odalc_emails(template_name, context_dict, recipient_list, sender=DEFAULT_EMAIL):
-	with open(os.path.join(EMAIL_TEMPLATES_PATH, template_name), 'r') as template:
+	with open(EMAIL_TEMPLATES_PATH, 'r') as template:
 		template_data = json.load(template)
 	context = Context(context_dict)
-	subject_template = Template(template_data['subject'])
-	body_template = Template(template_data['body'])
+	subject_template = Template(template_data[template_name]['subject'])
+	body_template = Template(template_data[template_name]['body'])
 	subject = subject_template.render(context)
 	body = body_template.render(context)
 	send_mail(subject, body, sender, recipient_list, fail_silently=False)
