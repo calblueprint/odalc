77937cc02d55c273cdec71cd3182ceec5d0f1d5e
akakitani <atsunori.kakitani@gmail.com>
2014-04-10 00:14:04 -0700

    Remove unused imports

--- odalc/base/urls.py
+++ odalc/base/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 from odalc.base.views import CourseDetailView, CourseEditView
 from odalc.students.views import SubmitCourseFeedbackView
 
