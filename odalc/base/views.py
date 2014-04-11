9c1e02b6298bf8593c177bc96c8adffdecf31996
vivekraghuram <vv9295@yahoo.com>
2014-04-10 22:23:28 -0700

    Add cases to template and add course title to page

--- odalc/base/views.py
+++ odalc/base/views.py
@@ -14,6 +14,7 @@ from odalc.teachers.models import TeacherUser
 from odalc.teachers.forms import EditCourseForm
 
 from django.core.urlresolvers import reverse_lazy
+from odalc.teachers.models import TeacherUser
 import stripe, json
 from django.conf import settings
 
@@ -62,11 +63,13 @@ class CourseDetailView(UserDataMixin, DetailView):
     def get_context_data(self, **kwargs):
         context = super(CourseDetailView, self).get_context_data(**kwargs)
         context['stripe_public_key'] = settings.STRIPE_PUBLIC_KEY
-        context['email'] = self.user.email
-        context['cost'] = self.object.cost
-        context['cost_in_cents'] = int(self.object.cost * 100)
-        context['room_left'] = self.object.students.count() < self.object.size
-        context['in_class'] = self.object.students.filter(id=self.user.id).exists()
+        if self.user.is_authenticated():
+            context['email'] = self.user.email
+            context['cost'] = self.object.cost
+            context['cost_in_cents'] = int(self.object.cost * 100)
+            context['course_full'] = self.object.students.count() >= self.object.size
+            context['in_class'] = self.object.students.filter(id=self.user.id).exists()
+            context['is_teacher'] = isinstance(self.user.child, TeacherUser)
         return context
 
     def dispatch(self, request, *args, **kwargs):
@@ -89,7 +92,7 @@ class CourseDetailView(UserDataMixin, DetailView):
         token = request.POST['stripeToken']
 
         # Create the charge on Stripe's servers - this will charge the user's card
-        if context['room_left']:
+        if context['course_full']:
             messages.error(request, "This course is already full. Your card hasn't been charged")
             return redirect('courses:detail',course.pk)
         if context['in_class']:
