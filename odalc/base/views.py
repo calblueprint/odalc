4211e70bbfce01e19c373ba128ee15dd983f73b1
akakitani <atsunori.kakitani@gmail.com>
2014-04-14 15:18:46 -0700

    Add mixin to get user data in views

--- odalc/base/views.py
+++ odalc/base/views.py
@@ -1,17 +1,43 @@
-from django.views.generic import DetailView, UpdateView, TemplateView, FormView, View
+from django.contrib.auth import login as auth_login, logout as auth_logout
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
+from django.core.urlresolvers import reverse_lazy
+from django.shortcuts import redirect
 from django.utils.decorators import method_decorator
 from django.views.decorators.debug import sensitive_post_parameters
-from django.contrib.auth import login as auth_login, logout as auth_logout
-from django.shortcuts import redirect
-from django.contrib import messages
-from odalc.base.models import Course
+from django.views.generic import DetailView, UpdateView, TemplateView, FormView, View
+
+from odalc.base.models import Course, User
+from odalc.odalc_admin.models import AdminUser
+from odalc.students.models import StudentUser
+from odalc.teachers.models import TeacherUser
 from odalc.teachers.forms import EditCourseForm
-from django.core.urlresolvers import reverse_lazy
 
 # Create your views here.
 
-class CourseDetailView(DetailView):
+class UserDataMixin(object):
+    def dispatch(self, request, *args, **kwargs):
+        self.user = request.user
+        if isinstance(self.user, User):
+            self.is_student_user = isinstance(self.user.child, StudentUser)
+            self.is_teacher_user = isinstance(self.user.child, TeacherUser)
+            self.is_admin_user = isinstance(self.user.child, AdminUser)
+            self.user = self.user.child
+        else:
+            self.is_student_user = False
+            self.is_teacher_user = False
+            self.is_admin_user = False
+        return super(UserDataMixin, self).dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(UserDataMixin, self).get_context_data(**kwargs)
+        context['is_student_user'] = self.is_student_user
+        context['is_teacher_user'] = self.is_teacher_user
+        context['is_admin_user'] = self.is_admin_user
+        return context
+
+
+class CourseDetailView(UserDataMixin, DetailView):
     model = Course
     context_object_name = 'course'
     template_name = 'base/course.html'
@@ -20,17 +46,17 @@ class CourseDetailView(DetailView):
         context = super(CourseDetailView, self).get_context_data(**kwargs)
         return context
 
-class CourseEditView(UpdateView):
+class CourseEditView(UserDataMixin, UpdateView):
     model = Course
     form_class = EditCourseForm
     context_object_name = 'course'
     template_name = 'base/course_edit.html'
     success_url = reverse_lazy('teachers:dashboard')
 
-class HomePageView(TemplateView):
+class HomePageView(UserDataMixin, TemplateView):
     template_name = 'base/home.html'
 
-class LoginView(FormView):
+class LoginView(UserDataMixin, FormView):
     template_name = 'base/login.html'
     form_class = AuthenticationForm
 
@@ -51,7 +77,7 @@ class LoginView(FormView):
         self.next_url = request.GET.get('next', 'home')
         return super(LoginView, self).dispatch(request, *args, **kwargs)
 
-class LogoutView(View):
+class LogoutView(UserDataMixin, View):
     def get(self, request, *args, **kwargs):
         auth_logout(request)
         return redirect('home')
