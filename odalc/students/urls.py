06fca0e0afefb481b15e46414ccd57323e2afda0
akakitani <atsunori.kakitani@gmail.com>
2014-04-14 15:19:21 -0700

    Fix for consistency in url names

--- odalc/students/urls.py
+++ odalc/students/urls.py
@@ -3,5 +3,5 @@ from odalc.students.views import StudentRegisterView, StudentDashboardView
 
 urlpatterns = patterns('',
     url(r'^register$', StudentRegisterView.as_view(), name='register'),
-    url(r'^$', StudentDashboardView.as_view(), name='student_dashboard'),
+    url(r'^$', StudentDashboardView.as_view(), name='dashboard'),
 )
