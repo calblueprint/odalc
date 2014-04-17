5623eba11ed5685ae7efc7b0f542a5123b6d19a3
John Du <johnldu@gmail.com>
2014-04-16 20:44:52 -0700

    adding admin permission to pages aayyyyy jdu++

--- odalc/base/views.py
+++ odalc/base/views.py
@@ -163,6 +163,7 @@ class LoginView(UserDataMixin, FormView):
         auth_login(self.request, form.get_user())
         if self.request.session.test_cookie_worked():
             self.request.session.delete_test_cookie()
+        print self.next_url
         return redirect(self.next_url)
 
     def form_invalid(self, form):
