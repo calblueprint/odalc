76b8f50c1f0a1f3de1114c67c3ab1a059e48d2a7
akakitani <atsunori.kakitani@gmail.com>
2014-04-16 22:44:52 -0700

    Add redirection from enroll button

--- odalc/settings.py
+++ odalc/settings.py
@@ -67,6 +67,17 @@ TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates'),
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    'django.core.context_processors.request',
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.contrib.messages.context_processors.messages",
+)
+
 ROOT_URLCONF = 'odalc.urls'
 
 WSGI_APPLICATION = 'odalc.wsgi.application'
