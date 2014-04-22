f91a8a8835bab02dfd79c32a119761354a47ff04
akakitani <atsunori.kakitani@gmail.com>
2014-04-21 19:52:23 -0700

    Add server-side logic to upload files to AWS

--- odalc/base/views.py
+++ odalc/base/views.py
@@ -1,9 +1,13 @@
+import time, json, base64, hmac, urllib
+from hashlib import sha1
+
 from django.contrib.auth import login as auth_login, logout as auth_logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
 from django.conf import settings
 from django.core.exceptions import PermissionDenied
 from django.core.urlresolvers import reverse_lazy
+from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.utils.decorators import method_decorator
 from django.views.decorators.debug import sensitive_post_parameters
@@ -184,3 +188,41 @@ class LogoutView(UserDataMixin, View):
     def get(self, request, *args, **kwargs):
         auth_logout(request)
         return redirect('home')
+
+
+class SignS3View(View):
+    def get(self, request, *args, **kwargs):
+        # Load necessary information into the application:
+        AWS_ACCESS_KEY = settings.AWS_ACCESS_KEY_ID
+        AWS_SECRET_KEY = settings.AWS_SECRET_ACCESS_KEY
+        S3_BUCKET = settings.S3_BUCKET
+        print AWS_ACCESS_KEY
+        print S3_BUCKET
+
+        # Collect information on the file from the GET parameters of the request:
+        object_name = urllib.quote_plus(request.GET.get('s3_object_name'))
+        mime_type = request.GET.get('s3_object_type')
+
+        # Set the expiry time of the signature (in seconds) and declare the permissions of the file to be uploaded
+        expires = int(time.time()+10)
+        amz_headers = "x-amz-acl:public-read"
+
+        # Generate the PUT request that JavaScript will use:
+        put_request = "PUT\n\n%s\n%d\n%s\n/%s/%s" % (mime_type, expires, amz_headers, S3_BUCKET, object_name)
+
+        # Generate the signature with which the request can be signed:
+        signature = base64.encodestring(hmac.new(AWS_SECRET_KEY, put_request, sha1).digest())
+        # Remove surrounding whitespace and quote special characters:
+        signature = urllib.quote_plus(signature.strip())
+
+        # Build the URL of the file in anticipation of its imminent upload:
+        url = 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET, object_name)
+        print url
+
+        content = json.dumps({
+            'signed_request': '%s?AWSAccessKeyId=%s&Expires=%d&Signature=%s' % (url, AWS_ACCESS_KEY, expires, signature),
+            'url': url
+        })
+
+        # Return the signed request and the anticipated URL back to the browser in JSON format:
+        return HttpResponse(content, content_type='text/plain; charset=x-user-defined')
