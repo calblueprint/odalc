5092b3662462787d0f13e39c6d7cb8afc8e62f3d
John Du <johnldu@gmail.com>
2014-04-24 23:08:39 -0700

    rm password_change

--- odalc/students/urls.py
+++ odalc/students/urls.py
@@ -1,11 +1,8 @@
 from django.conf.urls import patterns, include, url
 from odalc.students.views import StudentRegisterView, StudentDashboardView, StudentEditView
-from django.contrib.auth.views import password_change
 
 urlpatterns = patterns('',
     url(r'^register/$', StudentRegisterView.as_view(), name='register'),
     url(r'^edit/$', StudentEditView.as_view(), name='edit'),
-    url(r'^password_change/$', password_change, {'template_name': 'students/password_change.html',
-                                                 'post_change_redirect': 'students:dashboard'}),
     url(r'^$', StudentDashboardView.as_view(), name='dashboard'),
 )
