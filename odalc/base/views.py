13b76f83d39c9fa32b6ee076ed7594da8fb9392c
akakitani <atsunori.kakitani@gmail.com>
2014-04-24 19:13:13 -0700

    Use boto library for signatures. Also changed settings on AWS side.

--- odalc/base/views.py
+++ odalc/base/views.py
@@ -20,6 +20,7 @@ from odalc.odalc_admin.models import AdminUser
 from odalc.students.models import StudentUser
 from odalc.teachers.models import TeacherUser
 
+from boto import connect_s3
 import stripe
 
 
@@ -206,7 +207,7 @@ class SignS3View(View):
     def get(self, request, *args, **kwargs):
         # Load necessary information into the application:
         AWS_ACCESS_KEY = settings.AWS_ACCESS_KEY_ID.strip()
-        AWS_SECRET_KEY = settings.AWS_SECRET_ACCESS_KEY.strip()
+        AWS_SECRET_KEY = settings.AWS_SECRET_ACCESS_KEY.strip().encode('UTF-8')
         S3_BUCKET = settings.S3_BUCKET.strip()
 
         # Collect information on the file from the GET parameters of the request:
@@ -216,25 +217,34 @@ class SignS3View(View):
         # Set the expiry time of the signature (in seconds) and declare the permissions of the file to be uploaded
         expires = int(time.time()+10)
         amz_headers = "x-amz-acl:public-read"
+        amz_header_dict = {
+            'x-amz-acl': 'public-read'
+        }
 
         # Generate the PUT request that JavaScript will use:
         put_request = "PUT\n\n%s\n%d\n%s\n/%s/%s" % (mime_type, expires, amz_headers, S3_BUCKET, object_name)
 
+        print put_request
+        connection = connect_s3(AWS_ACCESS_KEY, AWS_SECRET_KEY)
+        signed_request = connection.generate_url(expires_in=10, method='PUT', bucket=S3_BUCKET, key=object_name, response_headers=amz_header_dict)
+        print signed_request
         # Generate the signature with which the request can be signed:
         signature = base64.encodestring(hmac.new(AWS_SECRET_KEY, put_request, sha1).digest())
         # Remove surrounding whitespace and quote special characters:
         signature = urllib.quote(signature.strip())
-
         # Build the URL of the file in anticipation of its imminent upload:
-        url = 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET, object_name)
 
         get_params = urllib.urlencode({
             'AWSAccessKeyId': AWS_ACCESS_KEY,
             'Expires': expires,
             'Signature': signature
         })
+        print get_params
+
+        url = 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET, object_name)
         content = json.dumps({
-            'signed_request': '%s?%s' % (url, get_params),
+            #'signed_request': signed_request,
+            'signed_request': '%s?AWSAccessKeyId=%s&Expires=%d&Signature=%s' % (url, AWS_ACCESS_KEY, expires, signature),
             'url': url
         })
 
