dd439fbd6acee975482b6e083d0e95147a2f9700
akakitani <atsunori.kakitani@gmail.com>
2014-05-06 23:28:32 -0700

    Better, valid seed data with cool pictures

--- odalc/base/management/commands/seed_data.py
+++ odalc/base/management/commands/seed_data.py
@@ -1,10 +1,10 @@
-import os
 import decimal
+from random import randint
+import os
+
 
 from django.contrib.auth.models import Group, Permission
 from django.core.management.base import BaseCommand
-from django.core.files import File
-from django.core.files.uploadedfile import SimpleUploadedFile
 
 from odalc.base.models import Course, CourseAvailability
 from odalc.odalc_admin.models import AdminUser
@@ -19,12 +19,17 @@ DIR = os.path.dirname(os.path.abspath(__file__))
 
 TEST_COURSE_IMAGE_URL = 'https://s3-us-west-1.amazonaws.com/odalc-stage-media/sample/sample_course_pic.jpg'
 TEST_TEACHER_IMAGE_URL = 'https://s3-us-west-1.amazonaws.com/odalc-stage-media/sample/sample_headshot.jpg'
+RANDOM_COURSE_IMAGE_URL = 'https://s3-us-west-1.amazonaws.com/odalc-stage-media/sample/sample_course_%d.JPG'
+RANDOM_TEACHER_IMAGE_URL = 'https://s3-us-west-1.amazonaws.com/odalc-stage-media/sample/sample_headshot_%d.JPG'
+
+
 TEST_PDF_URL = 'https://s3-us-west-1.amazonaws.com/odalc-stage-media/sample/sample_pdf.pdf'
 TEST_TEACHER_EMAIL = 'teacher@teacher.com'
 TEST_STUDENT_EMAIL = 'student@student.com'
 TEST_ADMIN_EMAIL = 'admin@admin.com'
 TEST_PASSWORD = 'odalc'
 COURSE_STATUS_CHOICES = [s[0] for s in Course.STATUS_CHOICES]
+COURSE_SKILL_CHOICES = [s[0] for s in Course.SKILL_CHOICES]
 
 class Command(BaseCommand):
     args = ''
@@ -51,7 +56,7 @@ class Command(BaseCommand):
                 zipcode='94709',
                 phone=self.sd.phone('es', 1),
                 about=self.sd.paragraph(),
-                picture=TEST_TEACHER_IMAGE_URL,
+                picture=RANDOM_TEACHER_IMAGE_URL % randint(1 , 10),
                 resume=TEST_PDF_URL,
                 experience='Here is my experience',
                 info_source='WEB'
@@ -127,6 +132,7 @@ class Command(BaseCommand):
         return
 
     def generate_courses(self, instances):
+        # Generate 'Pending' Courses
         for x in range(instances):
             student_qs = StudentUser.objects.all()
             course_students = []
@@ -140,16 +146,103 @@ class Command(BaseCommand):
                 short_description=self.sd.words(15, 20),
                 long_description=self.sd.paragraph(),
                 size=self.sd.int(8, 10),
-                start_datetime=self.sd.future_datetime(60, 1440),
-                end_datetime=self.sd.future_datetime(1440, 2880),
+                start_datetime=self.sd.future_datetime(1000, 3000),
+                end_datetime=self.sd.future_datetime(6000, 10000),
                 prereqs=self.sd.words(3, 7),
-                skill_level=Course.SKILL_BEGINNER,
+                skill_level=self.sd.choice(COURSE_SKILL_CHOICES),
                 cost=decimal.Decimal('6.00'),
                 odalc_cost_split=decimal.Decimal('2.50'),
-                image=TEST_COURSE_IMAGE_URL,
+                image=RANDOM_COURSE_IMAGE_URL % randint(1, 10),
                 course_material=TEST_PDF_URL,
                 additional_info=self.sd.paragraph(),
-                status=self.sd.choice(COURSE_STATUS_CHOICES)
+                status=Course.STATUS_PENDING
+            )
+            course.save()
+            for s in course_students:
+                course.students.add(s)
+            course.save()
+        # Generate 'Accepted' Courses
+        for x in range(instances):
+            student_qs = StudentUser.objects.all()
+            course_students = []
+            for x in range(6):
+                s = self.sd.db_object_from_queryset(student_qs)
+                course_students.append(s)
+                student_qs = student_qs.exclude(pk=s.pk)
+            course = Course(
+                teacher=self.sd.db_object(TeacherUser),
+                title=self.sd.words(1, 3),
+                short_description=self.sd.words(15, 20),
+                long_description=self.sd.paragraph(),
+                size=self.sd.int(8, 10),
+                start_datetime=self.sd.future_datetime(1000, 3000),
+                end_datetime=self.sd.future_datetime(6000, 10000),
+                prereqs=self.sd.words(3, 7),
+                skill_level=self.sd.choice(COURSE_SKILL_CHOICES),
+                cost=decimal.Decimal('6.00'),
+                odalc_cost_split=decimal.Decimal('2.50'),
+                image=RANDOM_COURSE_IMAGE_URL % randint(1, 10),
+                course_material=TEST_PDF_URL,
+                additional_info=self.sd.paragraph(),
+                status=Course.STATUS_ACCEPTED
+            )
+            course.save()
+            for s in course_students:
+                course.students.add(s)
+            course.save()
+        # Generate 'Finished' Courses
+        for x in range(instances):
+            student_qs = StudentUser.objects.all()
+            course_students = []
+            for x in range(6):
+                s = self.sd.db_object_from_queryset(student_qs)
+                course_students.append(s)
+                student_qs = student_qs.exclude(pk=s.pk)
+            course = Course(
+                teacher=self.sd.db_object(TeacherUser),
+                title=self.sd.words(1, 3),
+                short_description=self.sd.words(15, 20),
+                long_description=self.sd.paragraph(),
+                size=self.sd.int(8, 10),
+                start_datetime=self.sd.past_datetime(1000, 3000),
+                end_datetime=self.sd.past_datetime(6000, 10000),
+                prereqs=self.sd.words(3, 7),
+                skill_level=self.sd.choice(COURSE_SKILL_CHOICES),
+                cost=decimal.Decimal('6.00'),
+                odalc_cost_split=decimal.Decimal('2.50'),
+                image=RANDOM_COURSE_IMAGE_URL % randint(1, 10),
+                course_material=TEST_PDF_URL,
+                additional_info=self.sd.paragraph(),
+                status=Course.STATUS_FINISHED
+            )
+            course.save()
+            for s in course_students:
+                course.students.add(s)
+            course.save()
+        # Generate 'Denied' Courses
+        for x in range(instances):
+            student_qs = StudentUser.objects.all()
+            course_students = []
+            for x in range(6):
+                s = self.sd.db_object_from_queryset(student_qs)
+                course_students.append(s)
+                student_qs = student_qs.exclude(pk=s.pk)
+            course = Course(
+                teacher=self.sd.db_object(TeacherUser),
+                title=self.sd.words(1, 3),
+                short_description=self.sd.words(15, 20),
+                long_description=self.sd.paragraph(),
+                size=self.sd.int(8, 10),
+                start_datetime=self.sd.past_datetime(1000, 3000),
+                end_datetime=self.sd.past_datetime(6000, 10000),
+                prereqs=self.sd.words(3, 7),
+                skill_level=self.sd.choice(COURSE_SKILL_CHOICES),
+                cost=decimal.Decimal('6.00'),
+                odalc_cost_split=decimal.Decimal('2.50'),
+                image=RANDOM_COURSE_IMAGE_URL % randint(1, 10),
+                course_material=TEST_PDF_URL,
+                additional_info=self.sd.paragraph(),
+                status=Course.STATUS_DENIED
             )
             course.save()
             for s in course_students:
@@ -186,7 +279,7 @@ class Command(BaseCommand):
         print "Generating Admins..."
         self.generate_admin()
         print "Generating Courses..."
-        self.generate_courses(10)
+        self.generate_courses(6)
         print "Generating Course Feedback..."
         self.generate_course_feedback(10)
         print "Generating Course Availabilities..."
