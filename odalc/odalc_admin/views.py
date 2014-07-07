b1fa33d22c1ccf3d0119409a9a2683768664d821
akakitani <atsunori.kakitani@gmail.com>
2014-07-06 20:10:28 -0700

    Fix twitter text

--- odalc/odalc_admin/views.py
+++ odalc/odalc_admin/views.py
@@ -41,7 +41,7 @@ class ApplicationReviewView(UserDataMixin, UpdateView):
         context['course'] = course
         context['course_url'] = 'http://' + self.request.get_host() + reverse('courses:detail', args=(course.id,))
         context['facebook_share'] = 'http://www.facebook.com/sharer.php?u=' + context['course_url']
-        context['twitter_share'] = 'https://twitter.com/home?status=Check%20out%20this%20new%20course%20that%20just%20went%20live%20at%20Oakland%20Digital!%20'+ context['course_url'] + '%20%23OaklandDigitalCourses%20via%20@ODALC'
+        context['twitter_share'] = 'https://twitter.com/home?status=Check%20out%20this%20new%20course%20that%20just%20went%20live%20at%20Oakland%20Digital!%20'+ context['course_url'] + '%20%23OaklandDigital%20via%20@ODALC'
         context['google_share'] = 'https://plus.google.com/share?url=' + context['course_url']
 
         if '_approve' in self.request.POST:
