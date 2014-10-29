ab1876a8f3c6ce59264ac1fb3c36c43b19b9ed43
akakitani <atsunori.kakitani@gmail.com>
2014-10-29 15:40:50 -0700

    Reorganize settings

--- odalc/settings.py
+++ odalc/settings.py
@@ -8,42 +8,42 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 import os
-
 import dj_database_url
+
+#
+# Application environment flags
+#
 
 IS_STAGE = 'IS_STAGE' in os.environ
 IS_PROD = 'IS_PROD' in os.environ
 IS_HEROKU = IS_STAGE or IS_PROD
-
-# Hard-coded urls: kind of ugly but we need these for when we want to send links in emails
-SITE_URL = os.environ.get('SITE_URL', 'http://localhost:8000')
+TEMPLATE_DEBUG = not IS_PROD
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = not IS_PROD
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '%=4^u&zuw7teu$lka26@*rox*g=4tdw)nikp$w7!$n61lkw#vn'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = not IS_PROD
-
-TEMPLATE_DEBUG = not IS_PROD
-
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
 PROJECT_ROOT = os.path.abspath(
     os.path.join(os.path.dirname(__file__), ".."),
 )
+SETTINGS_PATH = os.path.dirname(os.path.realpath(__file__))
 
+
+#
 # Application definition
+#
 
-INSTALLED_APPS = (
+DJANGO_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'localflavor',
-    'athumb',
-    'djangobower',
-    'widget_tweaks',
+)
+
+ODALC_APPS = (
     'odalc.base',
     'odalc.courses',
     'odalc.odalc_admin',
@@ -51,6 +51,17 @@ INSTALLED_APPS = (
     'odalc.teachers',
     'odalc.users',
 )
+
+THIRD_PARTY_APPS = (
+    'athumb',
+    'djangobower',
+    'localflavor',
+    'pipeline',
+    'storages',
+    'widget_tweaks',
+)
+
+INSTALLED_APPS = DJANGO_APPS + ODALC_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -62,9 +73,6 @@ MIDDLEWARE_CLASSES = (
 )
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
     os.path.join(BASE_DIR, 'templates'),
 )
 
@@ -80,30 +88,34 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 )
 
 ROOT_URLCONF = 'odalc.urls'
-
 WSGI_APPLICATION = 'odalc.wsgi.application'
-
 AUTH_USER_MODEL = 'users.User'
 
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-# Parse database configuration from $DATABASE_URL
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'odalc_db',
-        'USER': 'odalc',
-        'PASSWORD': 'odalc',
-        'HOST': '127.0.0.1',
-        'PORT': '',
+#
+# Database setup
+#
+
+if IS_HEROKU:
+    DATABASES['default'] =  dj_database_url.config()
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'odalc_db',
+            'USER': 'odalc',
+            'PASSWORD': 'odalc',
+            'HOST': '127.0.0.1',
+            'PORT': '',
+        }
     }
-}
 
-SETTINGS_PATH = os.path.dirname(os.path.realpath(__file__))
 
-# Setup email backends - during development, write email out to console
-if "IS_STAGE" in os.environ or "IS_PROD" in os.environ:
+#
+# Email backends -  during development, write email out to console
+#
+
+if IS_HEROKU:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
@@ -114,25 +126,28 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'dev-odalc@odalc.org')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 EMAIL_USE_TLS = 'EMAIL_USE_TLS' in os.environ
 EMAIL_TEMPLATES_PATH = os.path.join(SETTINGS_PATH, 'templates', 'emails', 'emails.yml')
+# Hard-coded urls: kind of ugly but we need these for when we want to send links in emails
+SITE_URL = os.environ.get('SITE_URL', 'http://localhost:8000')
 
 
-# Stripe API Keys
+#
+# Stripe Config
+#
+
+# TODO: Set up these as environment vars
 STRIPE_SECRET_KEY = 'sk_test_jQUK6ubDrTtpW1i2ar4QFuMl'
 STRIPE_PUBLIC_KEY = 'pk_test_2IbsMYbzDjE6RKtGbVgPt7pK'
 
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+#
+# Internationalization and date/time
+#
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'America/Los_Angeles'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = False
-
 TIME_INPUT_FORMATS = (
     '%H:%M:%S',     # '14:30:59'
     '%H:%M:%S.%f',  # '14:30:59.000200'
@@ -140,19 +155,24 @@ TIME_INPUT_FORMATS = (
     '%I:%M%p',      # '2:30pm'
 )
 
-# djangobower settings
-BOWER_COMPONENTS_ROOT = os.path.join(BASE_DIR, 'static')
-BOWER_INSTALLED_APPS = (
-    'foundation',
-)
 
+#
+# Storage configs
+#
+
+# django-storage settings
 # If you don't want this to be the global default, just make sure you
 # specify the S3BotoStorage_AllPublic backend on a per-field basis.
 #DEFAULT_FILE_STORAGE = 'athumb.backends.s3boto.S3BotoStorage_AllPublic'
+#DEFAULT_FILE_STORAGE = 'odalc.lib.s3.S3BotoStorage_ODALC'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+STATICFILES_STORAGE = 'pipeline.storage.PipelineStorage'
 
-DEFAULT_FILE_STORAGE = 'odalc.lib.s3.S3BotoStorage_ODALC'
 
-# Amazon S3 Configs
+#
+# Amazon S3 configs
+#
+
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', '')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', '')
 S3_BUCKET = os.environ.get('S3_BUCKET', '')
@@ -160,42 +180,64 @@ AWS_STORAGE_BUCKET_NAME = S3_BUCKET
 AWS_REGION = ''
 MEDIA_CACHE_BUSTER = 'refetch'
 
-###################
-# Heroku Settings #
-###################
+
+#
+# Static assets configs
+#
 
 if IS_HEROKU:
-    DATABASES['default'] =  dj_database_url.config()
+    STATIC_URL = 'https://s3.amazonaws.com/{aws_bucket}/static/'.format(aws_bucket=AWS_STORAGE_BUCKET_NAME)
+else:
+    STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'djangobower.finders.BowerFinder',
+    'pipeline.finders.PipelineFinder',
+)
+
+
+#
+# User upload configs
+#
+
+if IS_PROD:
+    # TODO: Set up user uploaded files for production
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+    MEDIA_URL = '/uploads/'
+else:
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+    MEDIA_URL = '/uploads/'
+
+
+#
+# Bower Configs TODO: Move off Bower
+#
+
+BOWER_COMPONENTS_ROOT = os.path.join(BASE_DIR, 'static')
+BOWER_INSTALLED_APPS = (
+    'foundation',
+)
+
+
+#
+# Django-Pipeline settings
+#
+
+PIPELINE_DISABLE_WRAPPER = True
+
+
+#
+# Heroku Settings
+#
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
-
-# Static asset configuration
-STATIC_ROOT = 'staticfiles'
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-# User uploaded files
-if IS_PROD:
-    # TODO: Set up user uploaded files for production
-    MEDIA_URL = '/uploads/'
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
-else:
-    MEDIA_URL = '/uploads/'
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
-
-
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'djangobower.finders.BowerFinder',
-)
 
