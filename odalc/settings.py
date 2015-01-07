a1d0175961ac999889a063cdf07c0b352ec636bb
akakitani <atsunori.kakitani@gmail.com>
2015-01-06 18:01:51 -0800

    Add django-sslify

--- odalc/settings.py
+++ odalc/settings.py
@@ -63,6 +63,7 @@ THIRD_PARTY_APPS = (
 INSTALLED_APPS = DJANGO_APPS + ODALC_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE_CLASSES = (
+    'sslify.middleware.SSLifyMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
